db5cf55fadd8b6a444156b8fc5fdb321e8ac04a0
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-18 20:47:52 -0500

    task 0 corr 6

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,53 @@
 #!/usr/bin/python3
-"""Log parsing"""
+"""
+    Computes stdin input
+"""
+
 import sys
 
 
-def read():
-    """reading of each line that enters the system"""
-    read = sys.stdin.read().split('\n')
-    return read
+def parse_data(data):
+    data = data.replace(' "GET /projects/260 HTTP/1.1"',
+                        "").replace(' -', "").replace('[', "").replace(']', "")
+    if data.count("-") == 3:
+        data = data.replace('-', " ", 1)
+    array = data.split()
+    if len(array) != 5:
+        return
+    return array
 
 
-def recorrer():
-    """loop through the entire data array"""
-    cont = 0
-    result = {}
-    sum = 0
-    try:
-        for item in read():
-            for status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-                metrics = item.split(' ')
-                try:
-                    if(metrics[7] == str(status_code)):
-                        if (result.get(status_code) is None):
-                            result[status_code] = 1
-                        else:
-                            result[status_code] = result.get(status_code) + 1
-                        sum += int(metrics[8])
-                        cont += 1
-                except Exception:
-                    pass
-
-            if (cont == 10):
-                print_result(result, sum)
-                cont = 0
-    except KeyboardInterrupt:
-        pass
-
-    print_result(result, sum)
-
-
-def print_result(result, sum):
-    """print the information according to a format"""
-    if(result):
-        print("File size: {}".format(sum))
-        for key, value in sorted(result.items()):
-            print("{}: {}".format(key, value))
-
-if __name__ == "__main__":
-    recorrer()
+i = 0
+size = 0
+status_code = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+try:
+    for line in sys.stdin:
+        i += 1
+        array = parse_data(line)
+        if array:
+            size += int(array[4])
+            if array[3] in status_code.keys():
+                status_code[array[3]] += 1
+        if i == 10:
+            print("File size: {}".format(size))
+            for k in sorted(status_code):
+                if status_code[k] != 0:
+                    print("{}: {}".format(k, status_code[k]))
+            i = 0
+except Exception:
+    pass
+finally:
+    print("File size: {}".format(size))
+    for k in sorted(status_code):
+        if status_code[k] != 0:
+            print("{}: {}".format(k, status_code[k]))
+    i = 0
