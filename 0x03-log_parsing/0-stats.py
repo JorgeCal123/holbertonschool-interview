22f14b0d0128d592887a11d93dba712fc283980f
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-18 20:03:32 -0500

    task 0 corr 1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 """Log parsing"""
 import sys
-#print(lectura)
-#print(lectura[0])
 
-def lectura():
-    lectura = sys.stdin.read().split('\n')
-    return lectura
+
+def read():
+    """reading of each line that enters the system"""
+    read = sys.stdin.read().split('\n')
+    return read
 
 
 def recorrer():
+    """loop through the entire data array"""
     cont = 0
     result = {}
     sum = 0
     try:
-        for item in lectura():
+        for item in read():
             for status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
                 metrics = item.split(' ')
                 try:
                     if(metrics[7] == str(status_code)):
-                        if (result.get(status_code) == None):
+                        if (result.get(status_code) is None):
                             result[status_code] = 1
                         else:
                             result[status_code] = result.get(status_code) + 1
                         sum += int(metrics[8])
                         cont += 1
-                except:
+                except Exception:
                     pass
 
             if (cont == 10):
@@ -41,6 +42,7 @@ def recorrer():
 
 
 def print_result(result, sum):
+    """print the information according to a format"""
     if(result):
         print("File size: " + str(sum))
         for sts_code in result:
