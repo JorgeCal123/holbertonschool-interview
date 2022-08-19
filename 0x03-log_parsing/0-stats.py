e90dc7fb52cb2409b3375225536f5528f9282872
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-18 21:21:23 -0500

    task 0 corr 11234

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python3
 """Log parsing"""
-import signal
 import sys
 
 
-if __name__ == '__main__':
-    sum_file_size = 0
-    status_code = {'200': 0,
-                   '301': 0,
-                   '400': 0,
-                   '401': 0,
-                   '403': 0,
-                   '404': 0,
-                   '405': 0,
-                   '500': 0}
+def read():
+    """reading of each line that enters the system"""
+    read = sys.stdin.read().split('\n')
+    return read
 
+
+def recorrer():
+    """loop through the entire data array"""
+    cont = 0
+    result = {}
+    sum = 0
     try:
-        i = 0
-        for line in sys.stdin:
-            args = line.split()
-            if len(args) > 6:
-                status_line = args[-2]
-                file_size = args[-1]
-                sum_file_size += int(file_size)
-                if status_line in status_code:
-                    i += 1
-                    status_code[status_line] += 1
-                    if i % 10 == 0:
-                        print('File size: {}'.format(sum_file_size))
-                        for key, value in sorted(status_code.items()):
-                            if value != 0:
-                                print('{}: {}'.format(key, value))
+        for item in read():
+            for status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
+                metrics = item.split(' ')
+                try:
+                    if(metrics[7] == str(status_code)):
+                        if (result.get(status_code) is None):
+                            result[status_code] = 1
+                        else:
+                            result[status_code] = result.get(status_code) + 1
+                        sum += int(metrics[8])
+                        cont += 1
+                except Exception:
+                    pass
 
-        print('File size: {}'.format(sum_file_size))
-        for key, value in sorted(status_code.items()):
-            if value != 0:
-                print('{}: {}'.format(key, value))
-
+            if (cont == 10):
+                print_result(result, sum)
+                cont = 0
     except KeyboardInterrupt:
-        print('File size: {}'.format(sum_file_size))
-        for key, value in sorted(status_code.items()):
-            if value != 0:
-                print('{}: {}'.format(key, value))
+        pass
+
+    print_result(result, sum)
+
+
+def print_result(result, sum):
+    """print the information according to a format"""
+    if(result):
+        print("File size: {}".format(sum))
+        for key, value in sorted(result.items()):
+            print("{}: {}".format(key, value))
+
+if __name__ == "__main__":
+    recorrer()
