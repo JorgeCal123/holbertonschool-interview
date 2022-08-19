73a2a097358d6ba90f8edc30513bdc847806d0e1
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-18 21:19:53 -0500

    task 0 corr 18

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,44 @@
 #!/usr/bin/python3
-"""Python script that reads stdin line by line and computes metrics"""
-
+"""Log parsing"""
+import signal
 import sys
 
 
-def print_n(t_file_size, status):
-    """Prints total file size and status list"""
-    print("File size: {:d}".format(t_file_size))
-    for key, value in sorted(status.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+if __name__ == '__main__':
+    sum_file_size = 0
+    status_code = {'200': 0,
+                   '301': 0,
+                   '400': 0,
+                   '401': 0,
+                   '403': 0,
+                   '404': 0,
+                   '405': 0,
+                   '500': 0}
 
+    try:
+        i = 0
+        for line in sys.stdin:
+            args = line.split()
+            if len(args) > 6:
+                status_line = args[-2]
+                file_size = args[-1]
+                sum_file_size += int(file_size)
+                if status_line in status_code:
+                    i += 1
+                    status_code[status_line] += 1
+                    if i % 10 == 0:
+                        print('File size: {}'.format(sum_file_size))
+                        for key, value in sorted(status_code.items()):
+                            if value != 0:
+                                print('{}: {}'.format(key, value))
 
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
+        print('File size: {}'.format(sum_file_size))
+        for key, value in sorted(status_code.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
 
-t_file_size = 0
-count = 0
-try:
-    for line in sys.stdin:
-        args = line.split()
-
-        if len(args) > 2:
-            status_code = args[-2]
-            file_size = int(args[-1])
-
-            if status_code in status:
-                status[status_code] += 1
-
-            t_file_size += file_size
-            count += 1
-
-            if count == 10:
-                print_n(t_file_size, status)
-                count = 0
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    print_n(t_file_size, status)
+    except KeyboardInterrupt:
+        print('File size: {}'.format(sum_file_size))
+        for key, value in sorted(status_code.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
