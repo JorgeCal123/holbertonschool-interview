7fcaac10b63d7db786c30f592de68f70d9cf1ca8
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-18 21:08:48 -0500

    task 0 corr 14

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python3
-""" Script that reads stdin line by line and computes metrics."""
-
+"""Log Parser"""
 import sys
 
-dlist = {"size": 0,
-         "lines": 1}
 
-errors = {"200": 0, "301": 0, "400": 0, "401": 0,
-          "403": 0, "404": 0, "405": 0, "500": 0}
+if __name__ == '__main__':
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
 
-def printf():
-    """ Print codes and numbers"""
-    print("File size: {}".format(dlist["size"]))
-    for key in sorted(errors.keys()):
-        if errors[key] != 0:
-            print("{}: {}".format(key, errors[key]))
+    def parse_line(line):
+        """ Checks the line for matches """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
 
-
-def datasize(data):
-    """ Count file codes and size"""
-    dlist["size"] += int(data[-1])
-    if data[-2] in errors:
-        errors[data[-2]] += 1
-
-
-if __name__ == "__main__":
+    linenum = 1
     try:
         for line in sys.stdin:
-            try:
-                datasize(line.split(" "))
-            except:
-                pass
-            if dlist["lines"] % 10 == 0:
-                printf()
-            dlist["lines"] += 1
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
     except KeyboardInterrupt:
-        printf()
+        print_stats()
         raise
-    printf()
+    print_stats()
