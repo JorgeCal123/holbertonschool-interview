e5c6a6d0721d6093b34ab7f4d318877b82e8dff6
JorgeCal123 <ingsistemas68@gmail.com>
2022-09-01 20:29:54 -0500

    task 0

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""N Queens"""
+import sys
+
+
+def nQueens(N, r, queenList):
+    """ method nQuens"""
+    for i in range(N):
+        flag = 0
+        for j in queenList:
+            if abs(i - j[1]) == abs(r - j[0]):
+                flag = 1
+                break
+            if i == j[1]:
+                flag = 1
+                break
+        if flag == 0:
+            queenList.append([r, i])
+            if r != N - 1:
+                nQueens(N, r + 1, queenList)
+            else:
+                print(queenList)
+            queenList.pop()
+
+if __name__ == "__main__":
+    """method main"""
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    if isinstance(sys.argv[1], int):
+        print("N must be a number")
+        exit(1)
+
+    N = int(sys.argv[1])
+    if (N < 4):
+        print("N must be at least 4")
+        exit(1)
+
+    r = 0
+    queenList = []
+    nQueens(N, r, queenList)
