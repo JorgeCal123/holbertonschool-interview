4df010b8db889685fd8d1c0fee25f401ba0d1644
JorgeCal123 <ingsistemas68@gmail.com>
2022-09-10 15:09:17 -0500

    task 0 api crr 8

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-const idFilm = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${idFilm}`;
 
-request(url, async (error, response, body) => {
-  if (!error) {
+const idFilm = process.argv[2];
+
+const url = `https://swapi.dev/api/films/${idFilm}/`;
+
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
     const characters = JSON.parse(body).characters;
-    for (const charId in characters) {
-      await new Promise((resolve, reject) => {
-        request(characters[charId], (error, response, body) => {
-          if (!error) {
-            console.log(JSON.parse(body).name);
-            resolve();
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
           }
         });
       });
+      console.log(res);
     }
   }
 });
