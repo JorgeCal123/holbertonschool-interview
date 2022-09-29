f7fd09cd8570ca04266fe780db1ab39b8c57de1f
JC <JC>
2022-09-29 08:26:10 -0500

    all task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""method island perimeter"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island"""
+    perimeter = 0
+    isla = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                isla += 1
+                "arriba"
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                "izq"
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                "der"
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                "abajo"
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+
+    return perimeter
