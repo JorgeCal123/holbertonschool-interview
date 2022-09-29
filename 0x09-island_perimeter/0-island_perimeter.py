b65dc39da2cdd50d213011b99b343eaf89451c6b
JC <JC>
2022-09-29 10:52:31 -0500

    all task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,16 +8,16 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                #arriba
+                "arriba"
                 if i - 1 < 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                #izq
+                "izq"
                 if j - 1 < 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                #der
+                "der"
                 if j + 1 >= len(grid[i]) or grid[i][j + 1] == 0:
                     perimeter += 1
-                #abajo
+                "abajo"
                 if i + 1 >= len(grid) or grid[i + 1][j] == 0:
                     perimeter += 1
 
