b68cdba05a26adca66db925d4b2f9fc8fad9f06a
JorgeCal123 <ingsistemas68@gmail.com>
2022-10-07 21:12:50 -0500

    all task

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,6 +3,34 @@
 
 
 def isWinner(x, nums):
-    for i in nums:
-        for j in range(1,nums[i]):
-            print(j)
+    """
+        method is winner
+    """
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    test = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not test[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            test[j] = False
+
+    test[0] = test[1] = False
+    c = 0
+    for i in range(len(test)):
+        if test[i]:
+            c += 1
+        test[i] = c
+
+    winner = ''
+    player1 = 0
+    for n in nums:
+        player1 += test[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        winner = None
+    if player1 * 2 > len(nums):
+        winner = "Maria"
+    else:
+        winner = "Ben"
+    return winner
